8b51f3fd3b2d871f196a573d08df94fe9a5975d0
ya-kyrylenko <ya.kyrylenko@gmail.com>
2014-08-25 01:36:57 +0300

    2 com

---
+++ scripts/js.js
@@ -0,0 +1,12 @@
+function CheckoutModel(){
+	this.literature = [
+		{name:"О компании QAP INT."},
+		{name:"Преимущества использования инструмента для Вашего бизнеса."},
+		{name:"Перспективы сотрудничества с QAP INT."},
+		{name:"Описание решения для разработки HTML-5 презентаций."},
+		{name:"Описание разработанного инструмента CLM2CRM и его интеграция с salesforce.com(русский)."},
+		{name:"Описание разработанного инструмента CLM2CRM и его интеграция с salesforce.com(english)."}
+	];
+};
+
+ko.applyBindings(new CheckoutModel());
