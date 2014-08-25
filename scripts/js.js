322200286cec07ec8c564432687e2fd52b841134
ya-kyrylenko <ya.kyrylenko@gmail.com>
2014-08-25 04:16:59 +0300

    working functionality

--- scripts/js.js
+++ scripts/js.js
@@ -7,6 +7,16 @@ function CheckoutModel(){
 		{name:"Описание разработанного инструмента CLM2CRM и его интеграция с salesforce.com(русский)."},
 		{name:"Описание разработанного инструмента CLM2CRM и его интеграция с salesforce.com(english)."}
 	];
+	this.mail = ko.observable();
+	this.theme = ko.observable();
+	this.textMessage = ko.observable();
+
+	this.selectedList = ko.observableArray([]);
+
+	this.filledData = ko.observable(false);
+	this.filledDataTrue = function(){
+		this.filledData(true);
+	}
 };
 
 ko.applyBindings(new CheckoutModel());
